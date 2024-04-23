cc266a8383e2e6f23b5d5c3f5598fe7764b73b5b
강창환 <lucas@gangchanghwan-ui-MacBookPro.local>
2024-04-23 12:01:34 +0900

    feat: 기존 배열 길이 변경 첫 소스 코드

--- array/chang-hwan/list.c
+++ array/chang-hwan/list.c
@@ -5,7 +5,7 @@
 int main (void)
 {
     // 1. 배열 A: 길이가 3인 int형 배열 생성.(값은 1, 2, 3)
-    int arrayA[3];
+    int *arrayA = malloc(3 * sizeof(int));
     // *arrayA = 1;
     // *(arrayA + 1) = 2;
     // *(arrayA + 2) = 3;
@@ -24,8 +24,10 @@ int main (void)
     // 4. 배열 B의 4번째 location에 value 4 insert.
     arrayB[3] = 4;
 
+    arrayA = arrayB;
+
     for (int i = 0; i < 4; i++)
     {
-         printf("배열 B의 %i번째 값: %i\n", i, *(arrayB + i));
+         printf("배열 A의 %i번째 값: %i\n", i, *(arrayA + i));
     }
 }
